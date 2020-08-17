efe352c78703a814792c23787a15673e9e57d73b
vaishnavikoranne <korannevaishnavi@gmail.com>
2020-08-17 14:27:51 +0530

    Update preparedata.go

--- functions/preparedata/preparedata.go
+++ functions/preparedata/preparedata.go
@@ -1,1 +1,46 @@
+package string
+
+import (
+	
+	
+	"strings"
+        "reflect"
+	"github.com/project-flogo/core/data"
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+func init() {
+	_ = function.Register(&arraytostring{})
+}
+
+type arraytostring struct {
+}
+
+func (s *arraytostring) Name() string {
+	return "arraytostring"
+}
+
+func (arraytostring) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{data.TypeAny, data.TypeString}, false
+}
+
+func (arraytostring) Eval(params ...interface{}) (interface{}, error) {
+	items:=params[0]
+	separator:=params[1].(string)
+        var paramSlice []string 
+	arrV := reflect.ValueOf(items)
+	for i:=0;i<arrV.Len();i++ {
+		inter:=arrV.Index(i).Interface()
+	
+                for _ , v := range inter.(map[string]interface{}){
+		   
+			paramSlice = append(paramSlice, v.(string) )
+		} 
+	}
+			
+	str:=strings.Join(paramSlice, separator)
+	return str, nil
+
+
+}
 
