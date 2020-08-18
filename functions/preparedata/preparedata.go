1c6ade99063a94c9adbd0e65a88d9b311df823b3
vaishnavikoranne <korannevaishnavi@gmail.com>
2020-08-18 15:39:13 +0530

    Update preparedata.go

--- functions/preparedata/preparedata.go
+++ functions/preparedata/preparedata.go
@@ -50,52 +50,11 @@ func (preparedata) Eval(params ...interface{}) (interface{}, error) {
 		fmt.Println(str)
         }
 	fstr:=strings.ReplaceAll(str, "%20", "+")
-	fstr:=strings.ReplaceAll(fstr, "%2B", "+")
-	fstr:=strings.TrimSuffix(fstr, "&")
+	fstr=strings.ReplaceAll(fstr, "%2B", "+")
+	fstr=strings.TrimSuffix(fstr, "&")
 	fmt.Println("%s", fstr)
 	
-	/*mapString := make(map[string]string)
-	for key, value := range items {
-		strKey := fmt.Sprintf("%v", key)
-		strValue := fmt.Sprintf("%v", value)
-
-		mapString[strKey] = strValue
-	}
-	fmt.Printf("%T\n", items)
-	fmt.Println("%v\n", items)
-	fmt.Println("%#v", items)
-	fmt.Printf("%#v", mapString)
-	*/
-	/*keys := make([]string, len(items))
-	i := 0
-	for k := range items {
-	  keys[i] = k
-	  fmt.Printf("%s\n", k)	
-	  i++
-	}*/
-	/*var paramSlice []string 
-	arrV := reflect.ValueOf(items)
-	for i:=0;i<arrV.Len();i++ {
-		inter:=arrV.Index(i).Interface()
 	
-                for _ , v := range inter.(map[string]interface{}){
-		   
-			paramSlice = append(paramSlice, v.(string) )
-		} 
-	}
-	separator:=params[1].(string)
-        var paramSlice []string 
-	arrV := reflect.ValueOf(items)
-	for i:=0;i<arrV.Len();i++ {
-		inter:=arrV.Index(i).Interface()
-	
-                for _ , v := range inter.(map[string]interface{}){
-		   
-			paramSlice = append(paramSlice, v.(string) )
-		} 
-	}
-			
-	str:=strings.Join(paramSlice, separator)*/
 	return fstr, nil
 
 
