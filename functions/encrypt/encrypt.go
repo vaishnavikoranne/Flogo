4167e2502c503ea357fc6160486aa6b93137a387
Vaishnavi Vasant Koranne <vkoranne@tibco.com>
2020-09-17 18:52:09 +0530

    encrypt together

--- functions/encrypt/encrypt.go
+++ functions/encrypt/encrypt.go
@@ -29,7 +29,7 @@ func (s *aesencrypt) Sig() (paramTypes []data.Type, isVariadic bool) {
 }
 
 
-func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+/*func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(ciphertext, padtext...)
@@ -64,15 +64,43 @@ func AESEncrypt(src string, sPassphrase string, saltString string) string {
 
 	return base64.StdEncoding.EncodeToString(initialVector) + base64.StdEncoding.EncodeToString(crypted)
 }
-
+*/
 
 func (s *aesencrypt) Eval(params ...interface{}) (interface{}, error) {
 
 	plainText := params[0].(string)
-	passphrase := params[1].(string)
-	saltstring:=params[2].(string)
-	fmt.Println(passphrase)
-	encryptedString := AESEncrypt(plainText, passphrase, saltstring)
+	sPassphrase := params[1].(string)
+	saltString:=params[2].(string)
+
+	fmt.Println(sPassphrase)
+	//encryptedString := AESEncrypt(plainText, passphrase, saltstring)
+	passphrase := []byte(sPassphrase)
+	salt := []byte(saltString)
+
+	key := pbkdf2.Key([]byte(passphrase), salt, 1000, 32, sha1.New)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println("key error1", err)
+	}
+	if plainText == "" {
+		fmt.Println("plain content empty")
+	}
+	//initialVector := make([]byte, aes.BlockSize)
+	var initialVector = []byte{34, 35, 35, 57, 68, 4, 35, 36, 7, 8, 35, 23, 35, 86, 35, 23}
+
+	ecb := cipher.NewCBCEncrypter(block, []byte(initialVector))
+	content := []byte(plainText)
+	blockSize:=block.BlockSize()
+	padding := blockSize - len(content)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	
+	content = append(content, padtext...)
+	
+	crypted := make([]byte, len(content))
+	ecb.CryptBlocks(crypted, content)
+
+	encryptedString:=base64.StdEncoding.EncodeToString(initialVector) + base64.StdEncoding.EncodeToString(crypted)
+
 	fmt.Println(encryptedString)
 	return encryptedString, nil
 
