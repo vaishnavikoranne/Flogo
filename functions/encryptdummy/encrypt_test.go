0824c31a9c08344d2093afd9cd3d8544c5d0e68a
Vaishnavi Vasant Koranne <vkoranne@tibco.com>
2020-09-17 21:41:50 +0530

    adding encrypt function

---
+++ functions/encryptdummy/encrypt_test.go
@@ -0,0 +1,22 @@
+package encrypt
+import(
+ "fmt"
+ 
+ "testing"
+ 
+)
+var testfn=&aesencrypt{}
+var inputString1,in2,in3 string
+func init(){
+	
+	inputString1="ball"
+	in2="47cef24b-2b82-4ac4-a27c-fb0aca32baea"
+	in3="ac103458-fcb6-41d3-94r0-43d25b4f4ff4"
+}
+
+func TestMyFirstFlogoFunctionFn_Eval_1(t *testing.T){
+	outputString, _:=testfn.Eval(inputString1,in2,in3)
+	
+	fmt.Println("Output ",outputString)
+
+}
