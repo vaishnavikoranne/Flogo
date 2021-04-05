4c8f124c0ecfe65421eece28474ba67ca663195a
Vaishnavi Koranne <korannevaishnavi@gmail.com>
2021-04-05 15:10:29 +0530

    Add files via upload

---
+++ functions/arraytostring/arraytostring_test.go
@@ -0,0 +1,23 @@
+package string
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/project-flogo/core/data/expression/function"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var in = &fnDateTimeToTimeStamp{}
+
+func init() {
+	function.ResolveAliases()
+}
+
+func TestInt64Sample(t *testing.T) {
+	final, err := in.Eval()
+	assert.Nil(t, err)
+	//assert.Equal(t, int(579), final)
+	fmt.Printf("%v\n", final)
+}
